e1f1c77edc50111035a62847089b17110c519161
DexDevelop <hojing9552@hotmail.com>
2021-07-28 23:28:45 +0800

    Connected to backend

--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input } from 'reactstrap';
 import { connect } from 'react-redux';
 import { addItem } from '../actions/itemActions';
-import { v4 as uuid } from 'uuid';
+
 
 class ItemModal extends Component {
     state = {
@@ -24,7 +24,6 @@ class ItemModal extends Component {
         e.preventDefault();
 
         const newItem = {
-            id: uuid(),
             name: this.state.name
         }
         // Add item via addItem action
