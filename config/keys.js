72d9d01e1e14f2a1dd095339a6705bef2bd3aa7a
DexDevelop <hojing9552@hotmail.com>
2021-07-30 23:40:09 +0800

    register done

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI:'mongodb+srv://Dexter_01:Dexter95502@cluster0.1syig.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-}
--- server.js
+++ server.js
@@ -1,22 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const config = require('config');
 
-const items = require('./routes/api/items');
 
 const app = express();
 
+// Bodyparser Middleware
 app.use(express.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+// const db = require('./config/keys').mongoURI;
+const db = config.get('mongoURI');
 
 // Connect to Mongo
-mongoose.connect(db, {iseNewUrlParser: true})
+mongoose.connect(db, {iseNewUrlParser: true, useCreateIndex: true}) //Adding new mongo url parser
     .then(() => console.log('MongoDB Connect...'))
     .catch(err => console.log(err));
 
 // Use Routes
-app.use('/api/items', items);
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+
+// Server static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
     const port = process.env.PORT || 5000;
 
